8fc0a45617a61209a6a8b0c5bf2279610f9e660d
breno fraga <breno.anjos@bandtec.com.br>
2022-10-19 22:35:42 -0300

    a

--- projectfindr/projectfindr/src/main/java/findr/projectfindr/model/Contactor.java
+++ projectfindr/projectfindr/src/main/java/findr/projectfindr/model/Contactor.java
@@ -1,6 +1,7 @@
 package findr.projectfindr.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Table(name = "contactor")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Contactor extends User {
 
     @Id
